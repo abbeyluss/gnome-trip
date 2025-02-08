d0d65d6dfeda1bfb981380353d5f44b848017f67
bellacd2104 <daniello.i@northeastern.edu>
2025-02-08 00:30:43 -0500

    set up oauth

--- main.py
+++ main.py
@@ -15,12 +15,26 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(64)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
 client_id='bcbf028279894f78b9502f436ebf01d7'
 client_secret='f8cbebfebfea44b2badb2178c7977376'
 redirect_uri= 'http://localhost:5000/callback'
 scope = 'playlist-read-private, user-read-recently-played, playlist-modify-private' # search for scopes page in API documentation and modify as needed
+
+cache_handler = FlaskSessionCacheHandler(session)
+
+sp_oauth = SpotifyOAuth(
+    client_id=client_id,
+    client_secret=client_secret,
+    redirect_uri=redirect_uri,
+    scope=scope,
+    cache_handler=cache_handler,
+    show_dialog=True    # optional 
+)
+
+sp = Spotify(auth_manager=sp_oauth)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
