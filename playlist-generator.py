dbebe34295dfddea0e318e6e194e9f26f7f69ac4
bellacd2104 <daniello.i@northeastern.edu>
2025-02-08 15:34:45 -0500

    top_artists

--- playlist-generator.py
+++ playlist-generator.py
@@ -1,5 +1,12 @@
 import os
-import spotipy 
+
+from spotipy import *
 
 from main import sp
+
+# get values for top artists and tracks
+
+top_artists = sp.current_user_top_artists(limit=5, time_range='short_term')
+top_tracks = sp.current_user_top_tracks(limit=5, time_range='short_term')
+
 
