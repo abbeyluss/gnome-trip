31f553cc5ec90a8616bd6f4c6f72607de950ab20
bellacd2104 <daniello.i@northeastern.edu>
2025-02-07 23:56:49 -0500

    continuing main

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import os
 
-from flask import Flask
+from flask import Flask, session
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(64)
+
+client_id
+client_secret
+redirect_uri
+scope
 
 if __name__ == '__main__':
     app.run(debug=True)
